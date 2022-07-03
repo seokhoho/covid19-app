0a9474c750183b42ccef1aa7d00f46bd12c46e18
seokhoho <kreel9402@gmail.com>
2022-07-04 05:57:43 +0900

    Initial Commit

---
+++ COVID19/CovidDetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CovidDetailViewController.swift
+//  COVID19
+//
+//  Created by 정유진 on 2022/07/04.
+//
+
+import UIKit
+
+class CovidDetailViewController: UITableViewController {
+
+    @IBOutlet weak var newCaseCell: UITableViewCell!
+    @IBOutlet weak var totalCaseCell: UITableViewCell!
+    @IBOutlet weak var recoveredCell: UITableViewCell!
+    @IBOutlet weak var deathCell: UITableViewCell!
+    @IBOutlet weak var percentageCell: UITableViewCell!
+    @IBOutlet weak var overseasInflowCell: UITableViewCell!
+    @IBOutlet weak var regionalOutbreakCell: UITableViewCell!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ COVID19/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  COVID19
+//
+//  Created by 정유진 on 2022/07/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var totalCaseLabel: UILabel!
+    @IBOutlet weak var newCaseLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
