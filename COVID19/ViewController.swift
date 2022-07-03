82be3351d653be416d2b4fe76bdce4bfc367b103
seokhoho <kreel9402@gmail.com>
2022-07-04 06:01:24 +0900

    Covid Ui Build
    Label, Stack View, Pie Chart View, Static Table View

--- COVID19/ViewController.swift
+++ COVID19/ViewController.swift
@@ -6,10 +6,13 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
     @IBOutlet weak var totalCaseLabel: UILabel!
     @IBOutlet weak var newCaseLabel: UILabel!
+    @IBOutlet weak var pieChartView: PieChartView!
+    //차트 라이브러리 안에 포함되어 import Charts를 작성
     
     override func viewDidLoad() {
         super.viewDidLoad()
